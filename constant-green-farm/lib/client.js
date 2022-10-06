5099db39ef1a8ee1467efce9e8fbc0eb5c817b83
Luis Jean <ljmontreal1@gmail.com>
2022-10-06 19:28:47 -0400

    Update client.js

--- constant-green-farm/lib/client.js
+++ constant-green-farm/lib/client.js
@@ -1,5 +1,5 @@
 import sanityClient from '@sanity/client';
-import imageUrlBuilder from '@Sanity/image-url';
+import imageUrlBuilder from '@Sanity/image-Url';
 
 export const client = sanityClient({
     projectId: '763jxe6w',
