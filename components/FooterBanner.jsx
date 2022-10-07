b1ccf4a9edcbf872e9a3e0dd1396c4c6802ec012
Luis Jean <ljmontreal1@gmail.com>
2022-10-06 21:23:15 -0400

    Update FooterBanner.jsx

--- components/FooterBanner.jsx
+++ components/FooterBanner.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
 
-import { urlFor } from '../constant-green-farm/lib/client';
+import { urlFor } from '../constant-green-farm/lib/Client.js';
 
 
 const FooterBanner = ({ footerBanner }) => {
