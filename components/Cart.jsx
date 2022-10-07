cf4f45b5900709f9e446e6794a699887e300f218
Luis Jean <ljmontreal1@gmail.com>
2022-10-06 22:02:35 -0400

    Update Cart.jsx

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -5,7 +5,7 @@ import { TiDeleteOutline } from 'react-icons/ti';
 import toast from 'react-hot-toast';
 
 import { useStateContext } from '../pages/product/context/StateContext';
-import { urlFor } from '../constant-green-farm/lib/client.js';
+import { urlFor } from '../constant-green-farm/lib/client';
 import getStripe from '../constant-green-farm/lib/getStripe.js';
 
 const Cart = () => {
